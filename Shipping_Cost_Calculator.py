c74b40d6a8dc677e1454a1227be342bf6911d99c
Thushara-R-Achar <thusharaachar@gamil.com>
2024-11-18 16:59:49 +0530

    Modifed Shipping file

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,3 +10,6 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
+#Here is a new update by <Thushara-R-Achar>
+print(f"User Name: {Thushara-R-Achar}")
+
